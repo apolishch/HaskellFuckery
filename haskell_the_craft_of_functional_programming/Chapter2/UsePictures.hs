a3d48e0605b77da35efec97a75f3a985d9504fc5
Abraham <abe@gobalto.com>
2013-09-08 22:56:33 -0700

    chapter 2

---
+++ haskell_the_craft_of_functional_programming/Chapter2/UsePictures.hs
@@ -0,0 +1,18 @@
+module Chapter2Exercises where
+import Pictures
+
+rotateHorse, whiteHorse, singleCheckersSquare, checkersBoard :: Picture
+checkersSquare :: Picture -> Picture -> Picture
+rotateHorse = rotate horse
+
+whiteHorse = horse
+
+checkersSquare one two = above (beside one two) (beside two one)
+
+singleCheckersSquare = checkersSquare white black
+checkersBoard = checkersSquare (checkersSquare singleCheckersSquare singleCheckersSquare) (checkersSquare singleCheckersSquare singleCheckersSquare)
+
+horseVariantOne = checkersSquare whiteHorse (invertColour whiteHorse)
+horseVariantTwo = above (beside whiteHorse (invertColour whiteHorse)) (beside (flipV (invertColour whiteHorse)) (flipV whiteHorse))
+horseVariantThree = above (beside whiteHorse (invertColour whiteHorse)) (beside (flipH (flipV (invertColour whiteHorse))) (flipH (flipV whiteHorse)))
+horseVariantFour = above (beside whiteHorse (invertColour whiteHorse)) (beside (flipH (invertColour whiteHorse)) (flipH whiteHorse))
