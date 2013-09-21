5c721fb49c91dc1dfbe34423923e38ae16ad51bb
Abraham <abe@gobalto.com>
2013-09-20 23:48:47 -0700

    chapter4

--- haskell_the_craft_of_functional_programming/Chapter4/Chapter4.hs
+++ haskell_the_craft_of_functional_programming/Chapter4/Chapter4.hs
@@ -36,3 +36,63 @@ howManyOfFourEqual a b c d
     | b == c && b == d = 3
     | a == b || a == c || a == d || b == c || b == d = 2
     | otherwise = 0
+
+maxThreeOccurs :: Int -> Int -> Int -> (Int, Int)
+
+
+maxThreeOccurs a b c =
+    (maximal a b c, occurs (maximal a b c) a b c)
+    where
+        maximal a b c = max (max a b) c
+        occurs a b c d
+            | a == b && a == c && a == d = 3
+            | a == c && a == d = 2
+            | a == b && a == d = 2
+            | a == b && a == c = 2
+            | otherwise = 1
+
+data GameResult = Win |
+              Loss |
+              Draw
+data Move = Rock | Paper | Scissors
+            deriving(Show, Eq)
+
+beat, lose :: Move -> Move
+beat Rock = Paper
+beat Paper = Scissors
+beat _ = Rock
+
+lose Rock = Scissors
+lose Paper = Rock
+lose _ = Paper
+
+outcome :: Move -> Move -> GameResult
+outcome a b
+    |a == beat b = Win
+    |a == lose b = Loss
+    |otherwise = Draw
+
+data Temp = Cold | Hot
+            deriving (Eq, Show, Ord)
+
+data Season = Winter | Fall | Summer | Spring
+
+seasonalTemperature :: Season -> Temp
+seasonalTemperature Summer = Hot
+seasonalTemperature _ = Cold
+
+rangeProduct :: Int -> Int -> Int
+rangeProduct m n
+    |m < n = m * (rangeProduct (m+1) n)
+    |m == n = n
+    |otherwise = rangeProduct n m
+
+fac :: Int -> Int
+fac m = rangeProduct 1 m
+
+recursiveMultiply :: Int -> Int -> Int
+recursiveMultiply a b
+    |a == 1 = b
+    |otherwise = b + recursiveMultiply (a-1) b
+
+
